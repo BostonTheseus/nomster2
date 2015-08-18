98dd6adcf99e48afa598ddc58674f63bd829fe00
Robert Morris <obviouslyrob@gmail.com>
2015-08-18 23:02:08 +0000

    Added validations so that the title can't be blank.

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -11,8 +11,12 @@ class PlacesController < ApplicationController
 	end
 
 	def create
-		current_user.places.create(place_params)
-		redirect_to root_path
+	  @place = current_user.places.create(place_params)
+	  if @place.valid?
+	    redirect_to root_path
+	  else
+	    render :new, :status => :unprocessable_entity
+	  end
 	end
 
 	def show
@@ -24,22 +28,30 @@ class PlacesController < ApplicationController
 
 		  if @place.user != current_user
     		return render :text => 'Not Allowed', :status => :forbidden
+    	end
   	end
 
-	end
-
 	def update
-  		@place = Place.find(params[:id])
-		if @place.user != current_user
-			return render :text => 'Not Allowed', :status => :forbidden
-  end
+	  @place = Place.find(params[:id])
+
+	  if @place.user != current_user
+	    return render :text => 'Not Allowed', :status => :forbidden
+	  end
+
+	  @place.update_attributes(place_params)
+	  if @place.valid?
+	    redirect_to root_path
+	  else
+	    render :edit, :status => :unprocessable_entity
+	  end
+	end
 
 	def destroy
 	  @place = Place.find(params[:id])
 	    if @place.user != current_user
     		return render :text => 'Not Allowed', :status => :forbidden
   		end
-  		
+
 	  @place.destroy
 	  redirect_to root_path
 	end
