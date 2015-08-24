d1fa24c13fccff774bf7eee6eae7b1a5d3f0c49d
Robert Morris <obviouslyrob@gmail.com>
2015-08-24 16:55:27 +0000

    Added an image class and updated connections with places

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,7 +2,7 @@ class Place < ActiveRecord::Base
 	self.per_page = 5
 	belongs_to :user
 	has_many :comments
-	
+	has_many :images
 	geocoded_by :address
   	after_validation :geocode
 	
---
+++ db/migrate/20150824164640_create_images.rb
@@ -0,0 +1,11 @@
+class CreateImages < ActiveRecord::Migration
+  def change
+    create_table :images do |t|
+
+    	t.text :caption
+    	t.integer :place_id
+
+      t.timestamps
+    end
+  end
+end
