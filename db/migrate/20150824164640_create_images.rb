0275654cbabf1f847c37d621b1c3efe30a9881cc
vagrant <vagrant@precise32.(none)>
2015-08-25 18:50:27 +0000

    Fix modal

--- db/migrate/20150824164640_create_images.rb
+++ db/migrate/20150824164640_create_images.rb
@@ -1,13 +1,13 @@
 class CreateImages < ActiveRecord::Migration
   def change
     create_table :images do |t|
-
-    	t.text :caption
-    	t.integer :place_id
+      t.text :caption
+      t.integer :place_id
 
       t.timestamps
     end
+
+    add_index :images, :place_id
   end
 
-  add_index :photos, :place_id
 end
